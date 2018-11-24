503230aabbbd6c6c469919afce6dee0bf2838c09
Aviad Shikloshi <aviad.shikloshi@similarweb.com>
2018-11-24 18:10:01 +0200

    updating instance modules

--- main.tf
+++ main.tf
@@ -17,25 +17,18 @@ module "aws_vpc_module" {
 module "aws_instance_module" {
   source = "./modules/instance"
 
-  instance_type       = "t2.micro"
-  instance_ami        = "${var.instance_ami}"
-  instance_monitoring = false
-  instance_public_ip  = "${var.instance_public_ip}"
-  instance_key_name   = "shikloshi-aws"
-
-  #instance_security_groups = ["${module.aws_vpc_module.aws_vpc_id}"]
+  instance_type            = "t2.micro"
+  instance_ami             = "${var.instance_ami}"
+  instance_monitoring      = false
+  instance_public_ip       = "${var.instance_public_ip}"
+  instance_key_name        = "shikloshi-aws"
+  instance_subnet_id       = "${module.aws_vpc_module.aws_subnet_id}"
   instance_security_groups = ["${module.aws_vpc_module.aws_security_group_id}"]
 
   block_volume_size = "${var.block_volume_size}"
   block_volume_type = "${var.block_volume_type}"
 
-  instance_subnet_id = "${module.aws_vpc_module.aws_subnet_id}"
-
   tags {
-    Name = "my-new-instance"
+    Name = "default-instance" # to change this to be configurable or auto generated on run
   }
-}
-
-output "test_value" {
-  value = "this is a test value"
 }
